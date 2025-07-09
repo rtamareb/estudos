653b35f67c2a288383293d38cd6fc370f86acf27
rtamareb <rtamarebushi@gmail.com>
2025-07-09 15:25:00 -0300

    Traz o valor oposto do digitad

---
+++ opposite.py
@@ -0,0 +1,11 @@
+# Opposite
+
+#print ("Example #1")
+
+#print (f"The opposite number of {number} is {oppnumber}")
+
+def opposite(number):
+    return number * -1
+
+number = float(input("Type any number: "))
+print(f"The opposite of number {number} is {opposite(number)}")
