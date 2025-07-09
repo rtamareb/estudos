c2897cc682cf74f44571982a6b6b1a2fd21d377a
rtamareb <rtamarebushi@gmail.com>
2025-07-09 16:51:19 -0300

    Operadores matematicos

---
+++ operators.py
@@ -0,0 +1,24 @@
+# Operadores Matemáticos
+
+# Operações básicas: +, -, *, /, %, **
+# %  ==> Resto da divisão. Ex.: 5 % 2 = 1
+# ** ==> Exponenciação. 5 ** 2 = 25
+# Operadores de comparação: ==,  !=, <, <=, >, >=
+
+print ("Operações matemáticas básicas")
+print("  Soma ==> 2 + 2 = ", 2 + 2)
+print ("  Subtração ==> 3 - 1 = ", 3 - 1)
+print ("  Multiplicação ==> 3 * 2 = ", 3*2)
+print ("  Divisão ==> 16 / 4 = ", 16 / 4)
+print ("  Resto ==> 7 % 3 = ", 7 % 3)
+print ("  Exponenciação ==> 3 ** 5 = ", 3 ** 5)
+print ("")
+print ("Operadores de Comparação")
+print ("  Igualdade ==> 4 == 4", 4 == 4)
+print ("  Igualdade ==> 4 == 3", 4 == 3)
+print ("  Diferente ==> 4 != 3", 4 != 3)
+print ("  Diferente ==> 4 != 4", 4 != 4)
+print ("  Menor ==> 3 < 5", 3 < 5)
+print ("  Menor ou igual ==> 3 <= 5", 3 <= 5)
+print ("  Maior ==> 3 > 5", 3 > 5)
+print ("  Maior ou igual ==> 3 >= 5", 3 >= 5)
