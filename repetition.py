697b766ed8409e920817c2f5a6b103c299698dfd
rtamareb <rtamarebushi@gmail.com>
2025-07-09 16:55:09 -0300

    Repetição

---
+++ repetition.py
@@ -0,0 +1,14 @@
+# Repetir uma "String"
+
+print ("")
+print ("Repetir um número(integer ou float) n vezes")
+result = 3 * 3
+print (f"Resultado de 3 * 3 = {result}")
+print ("")
+result = 3 * 2.5
+print (f"Resultado de 3 * 2.5 = {result:.10f}")
+print ("")
+print ("Repetir uma string")
+result = 3 * "Kaxu"
+print (f"Resultado de 3 * Kaxu = {result}")
+print ("")
